52d37d837187ec56b9c0f0bfc5e8e1adb0aeae83
felipekako <felipekakosampaio@gmail.com>
2023-06-07 16:44:06 -0300

    messagem certa

--- server/app.js
+++ server/app.js
@@ -2,6 +2,9 @@ const express = require('express')
 const cors = require('cors')
 const app = express();
 
+app.get('/',(req,res)=>{
+    res.json({message: 'bem vindo a api '})
+})
 app.listen(3000,()=>{
     console.log('servidor rodando na porta 3000, para acessar use http://localhost:3000/')
 })
