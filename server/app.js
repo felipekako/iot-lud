c0b339ccd6eeb0f9b5401f454f6aa67581b36e38
felipekako <felipekakosampaio@gmail.com>
2023-06-07 16:39:24 -0300

    express

--- server/app.js
+++ server/app.js
@@ -1,1 +1,7 @@
-console.log('abcdefghi')
+const express = require('express')
+const cors = require('cors')
+const app = express();
+
+app.listen(3000,()=>{
+    console.log('servidor rodando na porta 3000, para acessar use http://localhost:3000/')
+})
