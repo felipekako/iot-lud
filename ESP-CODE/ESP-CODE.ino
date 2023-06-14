a49c062de077b9d6563ae9486160bd481727667b
felipekako <felipekakosampaio@gmail.com>
2023-06-14 17:02:38 -0300

    carregand

--- ESP-CODE/ESP-CODE.ino
+++ ESP-CODE/ESP-CODE.ino
@@ -1,7 +1,40 @@
+#include "./config.h"
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <ArduinoJson.h>
+
 #define led D8
+
+bool stateLedApi;
+
 void setup() {
+
+  Serial.println();
+  Serial.print("Conectando o wifi");
+  Serial.println(ssid);
+  WiFi.begin(ssid,password);
+  while(WiFi.status() != WL_CONNECTED){
+     delay(1000);
+  Serial.print('.');
+  }
+  Serial.println("");
+  Serial.println("wifi conectado");
   // put your setup code here, to run once:
   pinMode(led,OUTPUT);
+
+
+  WiFiClientSecure client;
+  HTTPClient http;
+
+  client.setInsecure();
+  http.begin(client,"https://iot-lead.onrender.com");
+  int httpCode = http.GET();
+  if(httpCode > 0){
+    String payload = http.getString();
+    Serial.println(payload);
+    }else{
+      Serial.print("erro ao entrar");
+      }
 }
 
 void loop() {
