f349b9a1054b36f85c5b0c7efe07c6cb45ca9ecd
felipekako <felipekakosampaio@gmail.com>
2023-06-21 16:49:00 -0300

    sensor dados

---
+++ server/models/sensor.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongose')
+
+const sensorSchema = new mongoose.Schema({
+    nome: String,
+    distancia: Number,
+    horario: {type: Date,default: Date.now}
+})
+
+const Sensor = mongoose.model("sensor",srnsorSchema)
+
+module.exports = Sensor;
